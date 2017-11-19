8aa203e205d0136fa51b94a592f59445b89dd3dd
Denis Efremov <efremov.a.denis@gmail.com>
2017-11-19 06:04:35 +0300

    Rewrited to classes

--- index.js
+++ index.js
@@ -4,28 +4,118 @@ const fs = require('fs')
 const Telegraf = require('telegraf')
 const { Markup, Extra } = require('telegraf')
 
-const app = new Telegraf(
-  process.env.BOT_TOKEN,
-  { username: process.env.BOT_NAME }
-)
+/**
+ * Class for telegraf state.
+ *
+ * @class  {TgState}
+ */
+class TgState
+{
 
-app.$options = {
-  pageSize: 10,
+  constructor (path, page = 0, options = {}) {
+    this.options = Object.assign(
+      { pageSize: 10 },
+      options
+    )
+
+    this.path = path
+    this.page = page
+
+    this.folderFiles = fs.readdirSync(path) || []
+
+    this.keyboard = new TgKeyboard(
+      this.folderFiles,
+      {
+        path,
+        page,
+        pageSize: this.options.pageSize,
+        pagesCount: this.pagesCount
+      }
+    )
+
+    return this
+  }
+
+  get length () {
+    return this.folderFiles.length
+  }
+
+  get pagesCount () {
+    return parseInt(this.length / this.options.pageSize)
+  }
+
+  get markup () {
+    return Extra.HTML().markup(markup =>
+      markup.inlineKeyboard(
+        this.keyboard.getKeyboard(this.page),
+        { wrap: (btn, index, currentRow) => currentRow.length <= 1 }
+      )
+    )
+  }
+
+  get stateInfo () {
+    return JSON.stringify({ path: this.path, page: this.page })
+  }
+
+  getReply (ctx) {
+    return ctx.reply(this.stateInfo, this.markup)
+  }
+
 }
 
-app.$state = {
-  path: '/home/den/Music',
-  page: 0,
-  folderButtons: [],
-  folderFiles: () => app.$state.folderButtons.map(button => button.text),
-  length: () => app.$state.folderButtons.length,
-  pagesCount: () => parseInt(app.$state.length() / app.$options.pageSize)
+/**
+ * Class for telegraf keyboard.
+ *
+ * @class  {TgKeyboard}
+ */
+class TgKeyboard
+{
+
+  constructor (files, options) {
+    this.files = files
+    this.options = options
+
+    return this
+  }
+
+  getKeyboard (page) {
+    this.options.page = page
+    return this.addNavigation(this.getPage())
+  }
+
+  getPage () {
+    return this.files.slice(
+      this.options.page * this.options.pageSize,
+      this.options.page * this.options.pageSize + this.options.pageSize
+    ).map(this.makeButton)
+  }
+
+  addNavigation (list) {
+    list.push(new TgButton('Next >', '/next', this.options.page === this.options.pagesCount))
+    list.push(new TgButton('Last >>>', '/last', this.options.page === this.options.pagesCount))
+
+    list = list.reverse()
+
+    list.push(new TgButton('< Prev', '/prev', this.options.page === 0))
+    list.push(new TgButton('<<< First', '/first', this.options.page === 0))
+
+    return list.reverse()
+  }
+
+  makeButton (file, idx) {
+    return new TgButton(file, `/get/${idx}`, false)
+  }
+
 }
 
-const readDir = (path) => fs.readdirSync(path)
-
+/**
+ * Class for telegraf button.
+ *
+ * @class  {TgButton}
+ */
 class TgButton
 {
+
   constructor (text, cb, hidden = false) {
     this.text = text
     this.callback_data = cb
@@ -33,95 +123,54 @@ class TgButton
 
     return this
   }
+
 }
 
-class TgMarkup
-{
-  constructor (buttons) {
-
+const app = new Telegraf(
+  process.env.BOT_TOKEN,
+  {
+    username: process.env.BOT_NAME,
   }
+)
 
-  wrapWithNavigation () {
+console.log(process.env.BOT_ROOT_FOLDER)
 
-  }
-}
-
-const addNavigationButtons = (list) => {
-  list.push(new TgButton('Next >', '/next', app.$state.page === app.$state.pagesCount()))
-  list.push(new TgButton('Last >>>', '/last', app.$state.page === app.$state.pagesCount()))
-
-  list = list.reverse()
-
-  list.push(new TgButton('< Prev', '/prev', app.$state.page === 0))
-  list.push(new TgButton('<<< First', '/first', app.$state.page === 0))
-
-  return list.reverse()
-}
-
-const makeButton = (file, idx) => new TgButton(file, `/get/${idx}`, false)
-
-const getKeyboard = (path, page) => {
-  if (!(app.$state.folderButtons && app.$state.folderButtons.length)) {
-    app.$state.folderButtons = readDir(path).map(makeButton)
-  }
-
-  return addNavigationButtons(
-    app.$state.folderButtons.slice(
-      page * app.$options.pageSize,
-      page * app.$options.pageSize + app.$options.pageSize
-    )
-  )
-}
-
-const getMarkup = (ctx) =>
-  Extra.HTML().markup(markup =>
-    markup.inlineKeyboard(
-      getKeyboard(ctx.path, ctx.page),
-      { wrap: (btn, index, currentRow) => currentRow.length <= 1 }
-    )
-  )
-
-const getStateInfo = (state) =>
-  JSON.stringify({ path: state.path, page: state.page })
-
-const getReply = (ctx) =>
-  ctx.reply(getStateInfo(app.$state), getMarkup(app.$state))
-
-const getAudioReply = (ctx) =>
-  ctx.replyWithAudio()
-
-const getState = () =>
-  app.$state
+const state = new TgState(process.env.BOT_ROOT_FOLDER)
 
 app.command('start', ctx =>
-  getReply(ctx)
+  state.getReply(ctx)
 )
 
 app.action('/next', ctx => {
-  app.$state.page += 1
-  return getReply(ctx)
+  state.page += 1
+  return state.getReply(ctx)
 })
 
 app.action('/prev', ctx => {
-  app.$state.page -= 1
-  return getReply(ctx)
+  state.page -= 1
+  return state.getReply(ctx)
 })
 
 app.action('/first', ctx => {
-  app.$state.page = 0
-  return getReply(ctx)
+  state.page = 0
+  return state.getReply(ctx)
 })
 
 app.action('/last', ctx => {
-  app.$state.page = app.$state.pagesCount()
-  return getReply(ctx)
+  state.page = state.pagesCount()
+  return state.getReply(ctx)
 })
 
 app.action(/^\/get\/(.*)$/, ctx => {
-  let filename = app.$state.folderFiles()[ctx.match[1]]
+  let filename = state.folderFiles[ctx.match[1]]
+
   console.log(`${filename} by ${ctx.from.first_name} ${ctx.from.last_name} @${ctx.from.username}`)
-  if (typeof filename === 'undefined') return ctx.reply('Error')
-  return ctx.replyWithAudio({ source: fs.readFileSync(`${app.$state.path}/${filename}`) })
+
+  if (typeof filename === 'undefined') {
+    return ctx.reply('Error')
+  }
+
+  return ctx.replyWithAudio({ source: fs.readFileSync(`${state.path}/${filename}`) })
 })
 
 app.startPolling()
